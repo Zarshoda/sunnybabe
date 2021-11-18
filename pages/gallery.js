48c1f51bd8f84753942944c126fd0dead569852b
Zarshoda <zariabdumalikova02@gmail.com>
2021-11-18 10:44:00 +0500

    234

--- pages/gallery.js
+++ pages/gallery.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Header from './header'
+import Image from 'next/image'
 
 const Gallery = () => {
     return (
@@ -8,67 +9,67 @@ const Gallery = () => {
             <div className='container'>
                 <div className="row pt-4">
                     <div className="col-lg-4 col-md-6 col-12 mb-3">
-                        <img src='img1.jpg' />
+                        <Image src='/img1.jpg' width='360' height='450' alt="di" />
                     </div>
                     <div className="col-lg-4 col-md-6 col-12 mb-3">
-                        <img src='img2.jpg' />
+                        <Image src='/img2.jpg' width='360' height='450' alt="di" />
                     </div>
                     <div className="col-lg-4 col-md-6 col-12 mb-3">
-                        <img src='img3.jpg' />
+                        <Image src='/img3.jpg' width='360' height='450' alt="di" />
                     </div>
                     <div className="col-lg-4 col-md-6 col-12 mb-3">
-                        <img src='img4.jpg' />
+                        <Image src='/img4.jpg' width='360' height='450' alt="di" />
                     </div>
                     <div className="col-lg-4 col-md-6 col-12 mb-3">
-                        <img src='img5.jpg' />
+                        <Image src='/img5.jpg' width='360' height='450' alt="di" />
                     </div>
                     <div className="col-lg-4 col-md-6 col-12 mb-3">
-                        <img src='img6.jpg' />
+                        <Image src='/img6.jpg' width='360' height='450' alt="di" />
                     </div>
                     <div className="col-lg-4 col-md-6 col-12 mb-3">
-                        <img src='img7.jpg' />
+                        <Image src='/img7.jpg' width='360' height='450' alt="di" />
                     </div>
                     <div className="col-lg-4 col-md-6 col-12 mb-3">
-                        <img src='img8.jpg' />
+                        <Image src='/img8.jpg' width='360' height='450' alt="di" />
                     </div>
                     <div className="col-lg-4 col-md-6 col-12 mb-3">
-                        <img src='img9.jpg' />
+                        <Image src='/img9.jpg' width='360' height='450' alt="di" />
                     </div>
                     <div className="col-lg-4 col-md-6 col-12 mb-3">
-                        <img src='img10.jpg' />
+                        <Image src='/img10.jpg' width='360' height='450' alt="di" />
                     </div>
                     <div className="col-lg-4 col-md-6 col-12 mb-3">
-                        <img src='img11.jpg' />
+                        <Image src='/img11.jpg' width='360' height='450' alt="di" />
                     </div>
                     <div className="col-lg-4 col-md-6 col-12 mb-3">
-                        <img src='img12.jpg' />
+                        <Image src='/img12.jpg' width='360' height='450' alt="di" />
                     </div>
                     <div className="col-lg-4 col-md-6 col-12 mb-3">
-                        <img src='img13.jpg' />
+                        <Image src='/img13.jpg' width='360' height='450' alt="di" />
                     </div>
                     <div className="col-lg-4 col-md-6 col-12 mb-3">
-                        <img src='img14.jpg' />
+                        <Image src='/img14.jpg' width='360' height='450' alt="di" />
                     </div>
                     <div className="col-lg-4 col-md-6 col-12 mb-3">
-                        <img src='img15.jpg' />
+                        <Image src='/img15.jpg' width='360' height='450' alt="di" />
                     </div>
                     <div className="col-lg-4 col-md-6 col-12 mb-3">
-                        <img src='img16.jpg' />
+                        <Image src='/img16.jpg' width='360' height='450' alt="di" />
                     </div>
                     <div className="col-lg-4 col-md-6 col-12 mb-3">
-                        <img src='img17.jpg' />
+                        <Image src='/img17.jpg' width='360' height='450' alt="di" />
                     </div>
                     <div className="col-lg-4 col-md-6 col-12 mb-3">
-                        <img src='img18.jpg' />
+                        <Image src='/img18.jpg' width='360' height='450' alt="di" />
                     </div>
                     <div className="col-lg-4 col-md-6 col-12 mb-3">
-                        <img src='img19.jpg' />
+                        <Image src='/img19.jpg' width='360' height='450' alt="di" />
                     </div>
                     <div className="col-lg-4 col-md-6 col-12 mb-3">
-                        <img src='img20.jpg' />
+                        <Image src='/img20.jpg' width='360' height='450' alt="di" />
                     </div>
                     <div className="col-lg-4 col-md-6 col-12 mb-3">
-                        <img src='img21.jpg' />
+                        <Image src='/img21.jpg' width='360' height='450' alt="di" />
                     </div>
                 </div>
             </div>
